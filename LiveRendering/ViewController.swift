065097ee7142d763560811630ce22bb7fc8fdeb9
Bruno Cortez <bruno.mcortez@gmail.com>
2020-03-04 22:00:12 -0300

    Initial Commit

---
+++ LiveRendering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveRendering
+//
+//  Created by Bruno Cortez on 3/4/20.
+//  Copyright © 2020 Bruno Cortez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
